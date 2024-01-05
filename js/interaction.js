3be80f49466bffe619b3610c4e4a6c3218dee4fa
Kasdein <kasdein@outlook.fr>
2024-01-05 09:37:35 +0000

    Initial commit

---
+++ js/interaction.js
@@ -0,0 +1,12 @@
+let getId = function (id) {
+    return document.getElementById(id)
+}
+let getClass = function (className, index) {
+    return document.getElementsByClassName(className)[index]
+}
+
+const continueButton = getId('continue')
+
+continueButton.onclick = () => {
+
+}
