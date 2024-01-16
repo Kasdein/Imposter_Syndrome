ca31968918685ed34e608ae0f802ca0ffd0f5faf
Kasdein <kasdein@outlook.fr>
2024-01-16 16:42:30 +0000

    sketch clickable door

--- js/interaction.js
+++ js/interaction.js
@@ -5,8 +5,3 @@ let getClass = function (className, index) {
     return document.getElementsByClassName(className)[index]
 }
 
-let buttonSketch = getClass('click')
-
-buttonSketch.onclick = () => {
-    window.open("https://Imposter_Syndrome/sketcbook_door.html")
-}
